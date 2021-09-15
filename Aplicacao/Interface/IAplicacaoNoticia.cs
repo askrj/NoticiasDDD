8429b971c60b706961fbecf37bc1ee4f8f2c9080
Alex Santos <ask.rj10@gmail.com>
2021-09-15 10:47:50 -0300

    camada de aplicação

---
+++ Aplicacao/Interface/IAplicacaoNoticia.cs
@@ -0,0 +1,17 @@
+﻿using Aplicacao.Interface.Genericos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao.Interfaces
+{
+    public interface IAplicacaoNoticia : IGenericaAplicacao<Noticia>
+    {
+        Task AdicionaNoticia(Noticia noticia);
+        Task AtualizaNoticia(Noticia noticia);
+        Task<List<Noticia>> ListarNoticiasAtivas();
+    }
+}
